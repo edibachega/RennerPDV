f00a4e6dc7295c0da1a3786c6e58c98f70732eec
edibachega <edibachega@gmail.com>
2016-11-17 21:42:24 -0200

    adicionado json

--- js/produtos.js
+++ js/produtos.js
@@ -1,18 +1,19 @@
 $('document').ready(function () {
     $.getJSON('/model/produtos.json',function (retorno) {
         $('#lista-produtos tbody').empty();
+        var total = 0;
         retorno.forEach(function (obj, idx){
-            console.log(obj, idx);
-            var preco = obj.preco.toString().split('.');
+            total += obj.quantidade;
             var tr = "<tr>"
                         +"<td>"+ obj.codigo +"</td>"
                         +"<td>"+ obj.nome +"</td>"
                         +"<td>"+ obj.marca +"</td>"
-                        +"<td>R$ "+ preco[0]+','+preco[1] +"</td>"
+                        +"<td>R$ "+ formataValor(obj.preco) +"</td>"
                         +"<td>"+ obj.quantidade +"</td>"
                     +"</tr>";
             $('#lista-produtos tbody').append(tr);
         });
+        $('#quantidade-total').html(total);
     }); 
     $('#codigo, #valor, #quantidade').keydown(function(key) {
         if(key.keyCode >= 48 && key.keyCode <= 57) {
@@ -26,3 +27,16 @@ $('document').ready(function () {
         }
     });
 });
+
+function formataValor(valor) {
+    var partes = valor.toString().split('.');
+    if(partes[1] == undefined){
+        partes[1] = '00';
+    }
+    if(partes[1].length < 2){
+        partes[1] += '0';
+    }
+    var retorno = partes[0] +','+ partes[1];
+    return retorno;
+}
+
